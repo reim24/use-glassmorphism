72220c3a9b14a4b5a0fb8e7d2ce015864eacc934
ReiMusabelli <musabellirei@gmail.com>
2024-01-26 16:53:01 +0100

    added demo

---
+++ src/index.tsx
@@ -0,0 +1,75 @@
+import ReactDOM from 'react-dom/client'
+import React, { useState } from 'react';
+import { useGlassmorphism } from '../lib/hooks/useGlassmorphism';
+import { GlassOptions } from '../lib/types';
+import "./index.css"
+const Demo = () => {
+  const [options, setOptions] = useState<GlassOptions>({
+    animation: {
+      duration: 0.5,
+      timingFunction: "ease-in-out",
+      delay: 0.1,
+    },
+  });
+  const glassRef = useGlassmorphism<HTMLDivElement>(options);
+
+  const handleChange = (key, value) => {
+    setOptions((prevOptions) => ({
+      ...prevOptions,
+      [key]: value,
+    }));
+  };
+
+
+  return (
+    <div className="glass-container">
+      <div className='ball' style={{ backgroundColor: "rgb(183 35 35)", top: '400px', left: "600px" }}></div>
+      <div className='ball' style={{ backgroundColor: "rgb(46, 48, 143)", top: '500px', right: "600px" }}></div>
+      <div ref={glassRef} className="content">
+        <h1>Glassmorphism Demo</h1>
+        <div>
+          <label>
+            Background Color:
+            <input
+              type="color"
+              value={options.color}
+              onChange={(e) => handleChange("color", e.target.value)}
+            />
+          </label>
+
+          <label>
+            Blur:
+            <input
+              type="range"
+              min={0}
+              max={20}
+              value={options.blur}
+              onChange={(e) => handleChange("blur", Number(e.target.value))}
+            />
+            {options.blur}
+          </label>
+
+          <label>
+            Transparency:
+            <input
+              type="range"
+              min={0}
+              max={1}
+              step={0.01}
+              value={options.transparency}
+              onChange={(e) => handleChange("transparency", Number(e.target.value))}
+            />
+            {options.transparency}
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Demo />
+)
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
-      name: "react-glassmorphism",
+      name: "use-glassmorphism",
 
-      fileName: "react-glassmorphism",
+      fileName: "use-glassmorphism",
     },
     rollupOptions: {
       external: ["react"],
     },
   },
   plugins: [
-    dts({ include: ["lib"], exclude: ["lib/utils"] })
+    dts({ include: ["lib"], exclude: ["lib/utils"] }),
+    react()
   ]
 });
