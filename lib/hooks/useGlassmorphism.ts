a34ee51f0f5b6f3ef8ba0e526769804d1b06b6fb
ReiMusabelli <musabellirei@gmail.com>
2024-01-26 16:52:48 +0100

    fix: default values

--- lib/hooks/useGlassmorphism.ts
+++ lib/hooks/useGlassmorphism.ts
@@ -9,33 +9,36 @@ import { GlassOptions } from "../types";
  * @returns {React.RefObject<T>} - The ref object to be assigned to the target element.
  */
 export const useGlassmorphism = <T extends HTMLElement>(
-  options: GlassOptions = { color: "rgb(255,255,255)", blur: 5 }
+  options: GlassOptions
 ): React.RefObject<T> => {
-  const { r, g, b, a } = parseColor(options.color);
+  const {
+    color = "rgb(255,255,255)",
+    blur = 5,
+    transparency,
+    animation,
+  } = options;
+
+  const { r, g, b, a } = parseColor(color);
   const elementRef = useRef<T | null>(null);
 
   useEffect(() => {
     const element = elementRef.current;
     if (!element) return;
 
-    if (options.animation) {
-      const {
-        duration = 0.3,
-        timingFunction = "ease",
-        delay = 0,
-      } = options.animation;
+    if (animation) {
+      const { duration = 0.3, timingFunction = "ease", delay = 0 } = animation;
 
       element.style.transition = `background ${duration}s ${timingFunction} ${delay}s, 
       backdrop-filter ${duration}s ${timingFunction} ${delay}s,
       box-shadow ${duration}s ${timingFunction} ${delay}s`;
+
     }
 
-    element.style.background = `rgba(${r}, ${g}, ${b}, ${options.transparency ?? a
-      })`;
+    element.style.background = `rgba(${r}, ${g}, ${b}, ${transparency ?? a})`;
     element.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.1)";
-    element.style.backdropFilter = `blur(${options.blur}px)`;
+    element.style.backdropFilter = `blur(${blur}px)`;
 
-  }, [elementRef, options]);
+  }, [elementRef, color, blur, transparency, animation]);
 
   return elementRef;
 };
